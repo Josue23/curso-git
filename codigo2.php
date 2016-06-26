e9c8d7507f17e222e9cee3c53acdc14eb75d7315
josue23 <josuerodrigues@gmail.com>
2016-06-26 19:49:20 -0300

    adicionado codigo2.php

---
+++ codigo2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<p><span>Consectetur vel rem deserunt praesentium libero impedit ab id assumenda illum autem, iusto necessitatibus accusamus maxime repellendus nam sint. Labore labore excepturi sint quas eius sapiente! Suscipit fuga natus earum.</span></p>
+	<p><span>Elit voluptate doloribus mollitia sed suscipit rem repudiandae veniam eaque adipisci soluta dicta aspernatur quae? Officia expedita aperiam repudiandae aperiam aliquam voluptatum quasi vero? Illo esse aliquam iure vitae sed.</span></p>
+	<p><span>Consectetur quo porro aut assumenda qui ipsa facilis. Consequuntur quam illo explicabo dolore modi? Vel libero dolores quis earum perferendis sequi alias perferendis maiores amet laudantium voluptates ex tempore odio!</span></p>
+	<p><span>Sit fugiat aliquam aspernatur fuga illum? Nam tempore quo ipsum doloribus fuga nam consequuntur tenetur earum pariatur ex iure, quia. Ex impedit iusto deleniti architecto tenetur eum dignissimos, repellat eaque.</span></p>
+	
+</body>
+</html>
